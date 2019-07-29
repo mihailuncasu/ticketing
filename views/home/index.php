89c35043b1303f0c3b8c9132d24e5ec3113119f4
Luncasu Mihai <mihai.luncasu.liviu@gmail.com>
2019-07-29 04:46:29 +0300

    last update

--- views/home/index.php
+++ views/home/index.php
@@ -1,4 +1,6 @@
 <div class="container text-center">
     <h1 class="cover-heading">Cover page.</h1>
     <p class="lead">Ticketing application. Application name: <?= APP_TITLE ?>. Enjoy!</p>
+    <br>
+    <p>Be sure you have departments in departments table first.</p>
 </div>
